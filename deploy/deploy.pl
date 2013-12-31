411a99416d593f5f9b99a3896849953082317b12
Takanao Yoshii <tyoshii716@gmail.com>
2014-01-01 01:11:39 +0900

    update deploy script

--- deploy/deploy.pl
+++ deploy/deploy.pl
@@ -9,6 +9,7 @@ my $list = 'bms.list';
 
 my @files = `cat $list`;
 
+my $hash;
 for my $row ( @files ) {
 
     chomp $row;
@@ -18,22 +19,52 @@ for my $row ( @files ) {
 
     my ( $cmd, $orig, $dest ) = split(' ', $row);
 
-    $orig = File::Spec->rel2abs($orig);
-    $dest = File::Spec->rel2abs($dest);
+    $orig = File::Spec->rel2abs($orig) if $orig;
+    $dest = File::Spec->rel2abs($dest) if $dest;
+
+    if ( ! $hash->{$cmd} ) {
+        $hash->{$cmd} = [];
+    }
+
+    push @{ $hash->{$cmd} }, {
+        orig => $orig,
+        dest => $dest,
+    };
+}
+
+for my $hash_dir ( @{$hash->{'dir'}} ) {
+    my $dir = $hash_dir->{'orig'};
+
+    if ( -e $dir && -f $dir ) {
+        warn "already exists file: $dir";
+    }
+    elsif ( ! -d $dir ) {
+        mkdir $dir or die "Failed mkdir $dir : $!";
+    }
+}
     
+for my $hash_sym ( @{$hash->{'symlink'}} ) {
+
+    my $dest = $hash_sym->{'dest'};
+    my $orig = $hash_sym->{'orig'};
+
     if ( -f $dest || -l $dest || -d $dest ) {
         `rm -rf $dest`;
     }
 
-    if ( $cmd eq 'symlink' ) {
-        _symlink($orig, $dest);
+    _symlink($orig, $dest);
+}
+    
+for my $hash_copy ( @{$hash->{'copy'}} ) {
+
+    my $dest = $hash_copy->{'dest'};
+    my $orig = $hash_copy->{'orig'};
+
+    if ( -f $dest || -l $dest || -d $dest ) {
+        `rm -rf $dest`;
     }
-    elsif ( $cmd eq 'copy' ) {
-        _copy($orig, $dest)
-    }
-    else {
-        warn "not correct command\n";
-    }
+
+    _copy($orig, $dest)
 }
 
 sub _copy {

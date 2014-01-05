366fc49c2cf951a46aab2068428bb664e57331a9
Takanao Yoshii <tyoshii716@gmail.com>
2014-01-05 12:57:30 +0900

    add login form

--- bms/fuel/app/classes/controller/top.php
+++ bms/fuel/app/classes/controller/top.php
@@ -5,7 +5,29 @@ class Controller_Top extends Controller
 
 	public function action_index()
 	{
-		return Response::forge(View::forge('top/index.twig'));
-	}
+    $view = View::forge('top/index.twig');
 
+    // login form
+    $form = Fieldset::forge();
+
+    $form->add('username', 'アカウント', array('maxlength' => 8))
+      ->add_rule('required')
+      ->add_rule('max_length', 8);
+
+    $form->add('password', 'パスワード', array('type' => 'password'))
+      ->add_rule('required')
+      ->add_rule('max_length', 8);
+
+    $form->add('submit', '', array('type' => 'submit', 'value' => 'ログイン'));
+
+    $form->repopulate();
+
+    // ログイン認証
+    $auth = Auth::instance();
+    Auth::logout(); 
+
+    $view->set_safe('login_form', $form->build(Uri::create('/')));
+
+		return $view;
+	}
 }

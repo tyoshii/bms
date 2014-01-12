4e541f655a30501c1a606865f336128cd5220544
Takanao Yoshii <tyoshii716@gmail.com>
2014-01-12 10:46:17 +0900

    top page /

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,7 +1,8 @@
 <?php
 return array(
-  'top' => 'top/index',
-  '_root_'  => 'welcome/index',  // The default route
+  '_root_' => 'top/index',
+
+//  '_root_'  => 'welcome/index',  // The default route
   '_404_'   => 'welcome/404',    // The main 404 route
 
   'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),

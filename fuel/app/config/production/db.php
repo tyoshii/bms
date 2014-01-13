74402d7702262502602f927cdae545225b660bd5
Takanao Yoshii <tyoshii716@gmail.com>
2014-01-13 23:11:08 +0900

    production db setting

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -1,14 +1,11 @@
 <?php
-/**
- * The production database settings. These get merged with the global settings.
- */
 
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
+			'dsn'        => 'mysql:host=localhost;dbname=bms;port=3306',
+			'username'   => 'root',
+			'password'   => Config::get('password.mysql.development.root'),
 		),
 	),
 );

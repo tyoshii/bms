853d11e020ef68cfb4a9169301a7e341682729a7
Takanao Yoshii <tyoshii716@gmail.com>
2014-01-19 18:50:40 +0900

    add password

--- fuel/app/classes/controller/user.php
+++ fuel/app/classes/controller/user.php
@@ -13,16 +13,49 @@ class Controller_User extends Controller_Base
     }
   }
 
-  public function action_index($user)
+  public function action_info()
   {
-    $form = self::_get_user_form();
+    $form = self::_get_info_form();
 
     $view = View::forge('user.twig');
 
     return Response::forge($view);
   }
 
-  public function _get_user_form()
+  public function action_password()
+  {
+    $form = self::_get_info_form();
+
+    $view = View::forge('user.twig');
+
+    return Response::forge($view);
+  }
+
+  public function _get_password_form()
+  {
+    $form = Fieldset::forge('password', array(
+      'form_attributes' => array(
+        'class' => 'form',
+        'role'  => 'search',
+      ),
+    ));
+
+    $form->add('password1', '新しいパスワード', array('type' => 'password', 'class' => 'form-control', 'placeholder' => 'Password'))
+      ->add_rule('required')
+      ->add_rule('min_length', 8)
+      ->add_rule('max_length', 250);
+    
+    $form->add('password2', '同じものを', array('type' => 'password', 'class' => 'form-control', 'placeholder' => 'Password'))
+      ->add_rule('required')
+      ->add_rule('min_length', 8)
+      ->add_rule('max_length', 250);
+
+    $form->add('submit', '', array('type' => 'submit', 'class' => 'btn btn-warning', 'value' => '変更'));
+
+    return $form;
+  }
+
+  public function _get_info_form()
   {
     $form = Fieldset::forge('user', array(
       'form_attributes' => array(

ea5cbfb574c9fad60cf7df627679e33cc7299922
Takanao Yoshii <tyoshii716@gmail.com>
2014-03-10 10:17:58 +0900

    アカウントと選手の紐付け #72

---
+++ fuel/app/migrations/029_rename_field_user_id_to_username_in_players.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Fuel\Migrations;
+
+class Rename_field_user_id_to_username_in_players
+{
+	public function up()
+	{
+		\DBUtil::modify_fields('players', array(
+			'user_id' => array('name' => 'username', 'type' => 'int unsigned')
+		));
+	}
+
+	public function down()
+	{
+	\DBUtil::modify_fields('players', array(
+			'username' => array('name' => 'user_id', 'type' => 'int unsigned')
+		));
+	}
+}

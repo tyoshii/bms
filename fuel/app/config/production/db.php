a2fc5ce5c460e1c29e8b86b0fc9abe5b91810455
Takanao Yoshii <tyoshii716@gmail.com>
2014-03-16 22:21:13 +0900

    tab to space

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -1,16 +1,16 @@
 <?php
 
 return array(
-	'default' => array(
-		'connection'  => array(
+  'default' => array(
+    'connection'  => array(
       'host'       => 'bm-s.info',
       'port'       => '3306',
-			'dsn'        => 'mysql:host=bm-s.info;dbname=bms;port=3306',
-			'username'   => 'root',
-			'password'   => Config::get('password.mysql.production.root'),
-		),
-		'backup'  => array(
-			'username'   => 'bms_backup',
-		),
-	),
+      'dsn'        => 'mysql:host=bm-s.info;dbname=bms;port=3306',
+      'username'   => 'root',
+      'password'   => Config::get('password.mysql.production.root'),
+    ),
+    'backup'  => array(
+      'username'   => 'bms_backup',
+    ),
+  ),
 );

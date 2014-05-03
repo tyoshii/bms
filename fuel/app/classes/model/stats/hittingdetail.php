662787917d272e3e98491bcaf0e7a3852a2ff4b8
Takanao Yoshii <tyoshii716@gmail.com>
2014-05-03 18:02:58 +0900

    stats系のモデル・テーブルを追加 #15

---
+++ fuel/app/classes/model/stats/hittingdetail.php
@@ -0,0 +1,29 @@
+<?php
+
+class Model_Stats_Hittingdetail extends \Orm\Model
+{
+	protected static $_properties = array(
+		'id',
+		'player_id',
+		'game_id',
+		'bat_times',
+		'direction',
+		'kind',
+		'result_id',
+		'created_at',
+		'updated_at',
+	);
+
+	protected static $_observers = array(
+		'Orm\Observer_CreatedAt' => array(
+			'events' => array('before_insert'),
+			'mysql_timestamp' => false,
+		),
+		'Orm\Observer_UpdatedAt' => array(
+			'events' => array('before_update'),
+			'mysql_timestamp' => false,
+		),
+	);
+	protected static $_table_name = 'stats_hittingdetails';
+
+}

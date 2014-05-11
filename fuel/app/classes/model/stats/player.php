652272668bc629b81e1fbff96412f629771de678
Takanao Yoshii <tyoshii716@gmail.com>
2014-05-11 12:41:09 +0900

    stats_meta rename stats_player

--- fuel/app/classes/model/stats/player.php
+++ fuel/app/classes/model/stats/player.php
@@ -1,6 +1,6 @@
 <?php
 
-class Model_Stats_Meta extends \Orm\Model
+class Model_Stats_Player extends \Orm\Model
 {
 	protected static $_properties = array(
 		'id',
@@ -24,45 +24,45 @@ class Model_Stats_Meta extends \Orm\Model
 			'mysql_timestamp' => false,
 		),
 	);
-	protected static $_table_name = 'stats_meta';
+	protected static $_table_name = 'stats_players';
 
   public static function getStarter( $game_id, $team_id )
   {
-    $query = DB::select()->from(array(self::$_table_name, 'meta'));
+    $query = DB::select()->from(array(self::$_table_name, 'player'));
 
-    $query->join('players', 'LEFT')->on('meta.player_id', '=', 'players.id');
+    $query->join('players', 'LEFT')->on('player.player_id', '=', 'players.id');
 
     $query->where(array(
-      'meta.game_id' => $game_id,
-      'meta.team_id' => $team_id,
+      'player.game_id' => $game_id,
+      'player.team_id' => $team_id,
     ));
 
-    $query->order_by('meta.disp_order');
+    $query->order_by('player.disp_order');
 
     return $query->execute()->as_array();
   }
 
-  // - TODO metaから取るのおかしいので修正する
+  // - TODO playerから取るのおかしいので修正する
   // Starter情報はコントローラー側で持っているので
   // 野手成績と同様のロジックとする。
   // （player_idをキーとして配列を返して、twigにplayerとstatsの２つを渡す
   public static function getPitchingStats( $game_id, $team_id )
   {
-    $query = DB::select()->from(array(self::$_table_name, 'meta'));
+    $query = DB::select()->from(array(self::$_table_name, 'player'));
 
-    $query->join('players', 'LEFT')->on('meta.player_id', '=', 'players.id');
+    $query->join('players', 'LEFT')->on('player.player_id', '=', 'players.id');
     $query->join('stats_pitchings', 'LEFT')
-          ->on('meta.player_id', '=', 'stats_pitchings.player_id')
-          ->on('meta.game_id',   '=', 'stats_pitchings.game_id')
-          ->on('meta.team_id',   '=', 'stats_pitchings.team_id');
+          ->on('player.player_id', '=', 'stats_pitchings.player_id')
+          ->on('player.game_id',   '=', 'stats_pitchings.game_id')
+          ->on('player.team_id',   '=', 'stats_pitchings.team_id');
 
     $query->where(array(
-      'meta.game_id' => $game_id,
-      'meta.team_id' => $team_id,
+      'player.game_id' => $game_id,
+      'player.team_id' => $team_id,
     ));
-    $query->where('meta.position', 'LIKE', '%1%');
+    $query->where('player.position', 'LIKE', '%1%');
 
-    $query->order_by('meta.disp_order');
+    $query->order_by('player.disp_order');
 
     return $query->execute()->as_array();
   }
@@ -81,14 +81,14 @@ class Model_Stats_Meta extends \Orm\Model
       {
         if ( ! $player ) continue;
 
-        $meta = self::forge($ids + array(
+        $player = self::forge($ids + array(
           'disp_order' => $disp_order,
           'player_id'  => $player['player_id'],
           'order'      => $player['order'] ?: 0,
           'position'   => implode(',', $player['position']),
         ));
 
-        $meta->save();
+        $player->save();
       }
 
       DB::commit_transaction();

efeb87983dec7b047e181ec3775b7b145dc44a04
Takanao Yoshii <tyoshii716@gmail.com>
2014-05-11 16:35:12 +0900

    選手追加の際に背番号をリセットするように対応 Fixed #61

--- public/js/game/edit.js
+++ public/js/game/edit.js
@@ -382,13 +382,17 @@ function add_order(self, kind) {
   var $tr = $($('.player-tr')[0]);
   var $clone = $tr.clone(true);
 
+  // init number
+  $clone.find('td.number').text('');
+
   // init order
   if ( kind === 'last' ) {
-    var $last = $('.player-tr:last');
     // 最後に追加するときは、打順をインクリメント
     var last_order = $('.player-tr:last td.order').text();
     $clone.find('td.order').text(++last_order);
+
     // 元々最後だった行から削除ボタンを消す
+    var $last = $('.player-tr:last');
     $last.find('button.delete-order').remove();
   }
   else {

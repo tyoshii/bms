11404d30ac326c6397decc23274b6ae7c71431a5
Takanao Yoshii <tyoshii716@gmail.com>
2014-05-20 06:18:28 +0900

    トップページのviewを修正

--- fuel/app/classes/controller/base.php
+++ fuel/app/classes/controller/base.php
@@ -6,14 +6,7 @@ class Controller_Base extends Controller
 
   public function before()
   {
-    if ( Uri::segment(1) === 'login' )
-    {
-      $this->_login_form = self::_get_login_form(array('form_class' => 'form'));
-    }
-    else
-    {
-      $this->_login_form = self::_get_login_form();
-    }
+    $this->_login_form = self::_get_login_form();
 
     if ( Auth::check() ) {
       return;
@@ -45,24 +38,21 @@ class Controller_Base extends Controller
     return $res;
   }
 
-  static public function _get_login_form ($cond = array())
+  static public function _get_login_form ()
   {
-    $form_name  = isset($cond['form_name'])  ? $cond['form_name']  : 'login';
-    $form_class = isset($cond['form_class']) ? $cond['form_class'] : 'navbar-form navbar-right';
-
     // login form
     $form = Fieldset::forge('login', array(
       'form_attributes' => array(
         'class' => 'form',
-        'role'  => 'search',
+        'role'  => 'login',
       ),
     ));
 
-    $form->add('username', '', array('class' => 'form-control', 'placeholder' => 'Account'))
+    $form->add('username', 'ユーザー名', array('class' => 'form-control', 'placeholder' => 'Username'))
       ->add_rule('required')
       ->add_rule('max_length', 40);
 
-    $form->add('password', '', array('type' => 'password', 'class' => 'form-control', 'placeholder' => 'Password'))
+    $form->add('password', 'パスワード', array('type' => 'password', 'class' => 'form-control', 'placeholder' => 'Password'))
       ->add_rule('required')
       ->add_rule('min_length', 8)
       ->add_rule('max_length', 250);

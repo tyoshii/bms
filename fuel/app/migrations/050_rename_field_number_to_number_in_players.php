e62538aa89a09d0b8b5ba15f49f1505744628fcf
Takanao Yoshii <tyoshii716@gmail.com>
2014-05-21 06:37:16 +0900

    playersテーブルのnumberカラムをvarcharに変更 Fixed #34

---
+++ fuel/app/migrations/050_rename_field_number_to_number_in_players.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Fuel\Migrations;
+
+class Rename_field_number_to_number_in_players
+{
+	public function up()
+	{
+		\DBUtil::modify_fields('players', array(
+			'number' => array('name' => 'number', 'type' => 'varchar', 'constraint' => 8)
+		));
+	}
+
+	public function down()
+	{
+	\DBUtil::modify_fields('players', array(
+			'number' => array('name' => 'number', 'type' => 'int', 'constraint' => 11)
+		));
+	}
+}

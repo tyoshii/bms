99c0538d67c697c6f39f73829d8ff2cc08e47860
Takanao Yoshii <tyoshii716@gmail.com>
2014-05-23 15:22:22 +0900

    deploy right script

--- fuel/app/classes/controller/deploy.php
+++ fuel/app/classes/controller/deploy.php
@@ -2,8 +2,16 @@
 
 class Controller_Deploy extends Controller
 {
-	public function action_index()
-	{
-    error_log(var_export(Input::post(), true));
+  public function action_index()
+  {
+    $data = json_decode(Input::post('payload'), true);
+
+    echo $data['ref'];
+    if ( $data['ref'] === 'refs/heads/test' )
+    {
+        `cd /home/tyoshii/git/tyoshii/bms/`;
+        `git co master`;
+        `git pull origin mater`;
+    }
   }
 }

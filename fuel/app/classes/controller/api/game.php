b1ef59d78e3d55322ba45d3167e4e5131e064f6e
Takanao Yoshii <tyoshii716@gmail.com>
2014-05-25 17:34:13 +0900

    status update IF

--- fuel/app/classes/controller/api/game.php
+++ fuel/app/classes/controller/api/game.php
@@ -7,15 +7,15 @@ class Controller_Api_Game extends Controller_Rest
     parent::before();
   }
 
-  // game_id/team_id
+  // get post data ( and validation )
   private static function _getIds()
   {
     $val = Validation::forge();
-    $val->add('game_id')->add_rule('required');
-    $val->add('team_id')->add_rule('required');
+    $val->add('game_id', 'game_id')->add_rule('required');
+    $val->add('team_id', 'team_id')->add_rule('required');
 
     if ( ! $val->run() ) {
-      throw new Exception();
+      throw new Exception($val->show_errors());
     }
 
     $ids = $val->validated();
@@ -30,10 +30,18 @@ class Controller_Api_Game extends Controller_Rest
       }
 
       // Moderatorsだとして、自分のチームの試合ですか？
-      if ( $ids['game_id'] !== Model_Player::getMyTeamId() )
+      if ( $ids['team_id'] !== Model_Player::getMyTeamId() )
       {
         throw new Exception('権限がありません');
       } 
+    }
+
+    // check game status
+    $action = Request::main()->action;
+    $status = Model_Game::get_game_status($ids['game_id'], $ids['team_id']);
+    if ( $action !== 'updateStatus' and $status == 2 )
+    {
+      throw new Exception('既に成績入力を完了している試合です'); 
     }
 
     return $ids;
@@ -41,10 +49,17 @@ class Controller_Api_Game extends Controller_Rest
 
   public function post_updateStatus()
   {
-    $game = Model_Game::find(Input::post('id'));
-    $game->game_status = Input::post('status');
-    $game->save();
+    $ids = self::_getIds();
 
+    $ret = Model_Game::update_status(
+      $ids['game_id'],
+      $ids['team_id'],
+      Input::post('status')
+    );
+
+    if ( ! $ret )
+      throw new Exception('ステータスのアップデートに失敗しました');
+  
     return "OK";
   }
 
@@ -173,5 +188,4 @@ class Controller_Api_Game extends Controller_Rest
 
     echo 'OK';
   }
-
 }

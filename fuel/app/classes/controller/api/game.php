91d4259085428ac4ce6875fc6fed6ae357e5921c
Takanao Yoshii <tyoshii716@gmail.com>
2014-06-01 16:40:58 +0900

    メッセージ修正

--- fuel/app/classes/controller/api/game.php
+++ fuel/app/classes/controller/api/game.php
@@ -67,7 +67,7 @@ class Controller_Api_Game extends Controller_Rest
   {
     // 権限チェック
     if ( ! Auth::has_access('game.editall') )
-      return Response::forge('出場選手を編集する権限がありません', 403);
+      return Response::forge('スコアを編集する権限がありません', 403);
 
     $form = Fieldset::forge('score');
     $form->add_model(Model_Games_Runningscore::forge());

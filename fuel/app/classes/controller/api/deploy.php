bca40b79acf2810cddee90126520a3bbb46d049a
Takanao Yoshii <tyoshii716@gmail.com>
2014-06-08 18:43:45 +0900

    deploy api add staging auto deploy

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -6,39 +6,52 @@ class Controller_Api_Deploy extends Controller
   {
     $data = json_decode(Input::post('payload'), true);
 
+    // debug
     echo $data['ref']."\n";
+
     if ( $data['ref'] === 'refs/heads/master' )
     {
-      try {
-        chdir("/home/tyoshii/git/tyoshii/bms/");
+      self::_deploy('/home/tyoshii/git/tyoshii/bms/', 'master', 'production');
+    } 
+    if ( $data['ref'] === 'refs/heads/staging' )
+    {
+      self::_deploy('/home/tyoshii/git/tyoshii/bms_staging/', 'staging', 'staging');
+    } 
+  }
+  
+  private static function _deploy($git_dir, $branch, $fuel_env)
+  {
+    try {
+      chdir($git_dir);
 
-        // service out
-        `/usr/bin/env php oil r service:out`;
+      // service out
+      `FUEL_ENV={$fuel_env} /usr/bin/env php oil r service:out`;
 
-        // git-pull
-        `git checkout master`;
-        `git pull origin master`;
+      // git-pull
+      `git checkout $branch`;
+      `git pull origin $branch`;
 
-        // oil
-        `/usr/bin/env php oil r migrate:current`;
+      // oil
+      // stagingとproductionで同じデータを見ている場合、
+      // stagingでmigrateしたあとにproductionでmigrateするとエラーになる？
+      `FUEL_ENV={$fuel_env} /usr/bin/env php oil r migrate:current`;
 
-        // deploy
-        chdir("/home/tyoshii/git/tyoshii/bms/deploy/");
-        `/usr/bin/env perl deploy.pl force`;
+      // deploy
+      chdir("{$git_dir}/deploy/");
+      `/usr/bin/env perl deploy.pl force`;
 
-        // service in
-        chdir("/home/tyoshii/git/tyoshii/bms/");
-        `/usr/bin/env php oil r service:in`;
+      // service in
+      chdir($git_dir);
+      `FUEL_ENV={$fuel_env} /usr/bin/env php oil r service:in`;
 
-        echo "DEPLOY DONE";
-      
-      } catch (Exception $e) {
-        echo $e->getMessage();
+      echo "DEPLOY DONE";
+    
+    } catch (Exception $e) {
+      echo $e->getMessage();
 
-        // service in
-        chdir("/home/tyoshii/git/tyoshii/bms/");
-        `/usr/bin/env php oil r service:in`;
-      }
+      // service in
+      chdir($git_dir);
+      `FUEL_ENV={$fuel_env} /usr/bin/env php oil r service:in`;
     }
   }
 }

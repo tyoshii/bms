6e84ff7950476da851ef1e1429f7699229399f11
Takanao Yoshii <tyoshii716@gmail.com>
2014-06-13 11:52:33 +0900

    migrateにcurrentは不要だった

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -44,7 +44,7 @@ class Controller_Api_Deploy extends Controller
       // oil
       // stagingとproductionで同じデータを見ている場合、
       // stagingでmigrateしたあとにproductionでmigrateするとエラーになる？
-      `FUEL_ENV={$fuel_env} /usr/bin/env php oil r migrate:current`;
+      `FUEL_ENV={$fuel_env} /usr/bin/env php oil r migrate`;
 
       // deploy
       chdir("{$git_dir}/deploy/");

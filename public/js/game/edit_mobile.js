23f8a0a84beccad198bd4238dc297b75f1794079
Takanao Yoshii <tyoshii716@gmail.com>
2014-06-25 18:58:51 +0900

    打席追加/削除ボタン

--- public/js/game/edit_mobile.js
+++ public/js/game/edit_mobile.js
@@ -1,3 +1,4 @@
+// switch batter
 $("span[role=switch-batter]").click(function(){
   var type  = $(this).attr("type"),
       $root = $(this).parents("div.batter-result-wrapper"),
@@ -13,4 +14,38 @@ $("span[role=switch-batter]").click(function(){
 
   $("div[index="+hide_index+"]").hide();
   $("div[index="+show_index+"]").show();
+});
+
+// add/delete batter detail
+$("button.detail-add").click(function(){
+  $table = $(this).parents("table#batter-detail");
+  $base  = $table.find("tr.batter-detail-data:last");
+  index  = $base.attr("index");
+  PA = parseInt(index) + 1;
+
+  // クローンして初期化
+  var $clone = $base.clone(true);  
+
+  $clone.find("td.PA").text("第"+PA+"打席");
+  $clone.find("select").each(function(){
+    $(this).val(0);
+  });
+
+  // index increment
+  $clone.attr("index", PA);
+
+  // append
+  $clone.insertAfter($base);
+});
+
+$("button.detail-del").click(function(){
+  $table = $(this).parents("table#batter-detail");
+  $base  = $table.find("tr.batter-detail-data:last");
+  index  = $base.attr("index");
+
+  if ( parseInt(index) === 1 ) {
+    return true;
+  }
+
+  $base.remove();
 });

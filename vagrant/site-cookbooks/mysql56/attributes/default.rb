b1e272138f5237e249e2a1e2e8e9683a84cf855e
Takanao Yoshii <tyoshii716@gmail.com>
2014-07-03 23:14:02 +0900

    version up mysql

--- vagrant/site-cookbooks/mysql56/attributes/default.rb
+++ vagrant/site-cookbooks/mysql56/attributes/default.rb
@@ -1,4 +1,4 @@
-versions = "5.6.15-1.el6.x86_64"
+versions = "5.6.19-1.el6.x86_64"
 default['mysql']['versions']   = versions
 default['mysql']['file_name']  = "MySQL-#{versions}.rpm-bundle.tar"
 default['mysql']['remote_uri'] = "http://ftp.jaist.ac.jp/pub/mysql/Downloads/MySQL-5.6/MySQL-#{versions}.rpm-bundle.tar"

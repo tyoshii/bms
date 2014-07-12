aac5cc688ec123a333c0e93dc0f4dd54785f7fc1
Takanao Yoshii <tyoshii716@gmail.com>
2014-07-12 18:49:34 +0900

    extractだと直前の変数引き継いでしまうのでダメでした。array_key_existsでチェック

--- fuel/app/classes/model/stats/player.php
+++ fuel/app/classes/model/stats/player.php
@@ -96,14 +96,11 @@ class Model_Stats_Player extends \Orm\Model
       {
         if ( ! $player ) continue;
 
-        // player_id, order, position
-        extract($player);
-
         $player = self::forge($ids + array(
           'disp_order' => $disp_order,
-          'player_id'  => $player_id,
-          'order'      => $order ?: 0,
-          'position'   => isset($position) ? implode(',', $position ) : '',
+          'player_id'  => $player['player_id'],
+          'order'      => $player['order'] ?: 0,
+          'position'   => array_key_exists('position', $player) ? implode(',', $player['position'] ) : '',
         ));
 
         $player->save();

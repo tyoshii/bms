f01f32fdb5b06203fd1e5e028257a0094353d764
Takanao Yoshii <tyoshii716@gmail.com>
2014-08-10 16:49:24 +0900

    add config file default_timezone

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -94,7 +94,7 @@ return array(
 	 * default_timezone		optional, if you want to change the server's default timezone
 	 */
 	// 'server_gmt_offset'  => 0,
-	// 'default_timezone'   => null,
+    'default_timezone'   => 'Asia/Tokyo',
 
 	/**
 	 * Logging Threshold.  Can be set to any of the following:

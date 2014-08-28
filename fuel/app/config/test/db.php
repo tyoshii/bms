9257a6ed5bac7cb19f6a3ffb1eccc6b2ac3fd586
Takanao Yoshii <tyoshii716@gmail.com>
2014-08-29 00:15:58 +0900

    add unittest initial: controller_base test

--- fuel/app/config/test/db.php
+++ fuel/app/config/test/db.php
@@ -2,12 +2,12 @@
 
 return array(
 	'default' => array(
-      'connection'  => array(
+		'connection'  => array(
       'host'       => 'localhost',
       'port'       => '3306',
-			'dsn'        => 'mysql:host=localhost;dbname=bms_test;port=3306',
+      'database'   => 'bms',
 			'username'   => 'root',
-			'password'   => Config::get('password.mysql.test.root'),
+			'password'   => Config::get('password.mysql.development.root'),
 		),
 	),
 );

a0d2b48554b342d7aa3f64383d8d32173846c1c9
Takanao Yoshii <tyoshii716@gmail.com>
2014-09-01 04:53:50 +0900

    model tests initial commit

---
+++ fuel/app/tests/model/games/runningscore.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Tests for Model_Games_Runningscore
+ *
+ * @group App
+ * @group Model
+ * @group Model_Games_Runningscore
+ */
+class Test_Model_Games_Runningscore extends \Test_Model_Base
+{
+  public function setUp()
+  {
+  }
+
+  public function tearDown()
+  {
+  }
+
+  public function test_スキーマチェック()
+  {
+    $this->assertSchema();
+  }
+}

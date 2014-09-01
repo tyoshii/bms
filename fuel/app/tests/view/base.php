eaffcc1c612f25988197fc79750fb4b8b9606e6c
Takanao Yoshii <tyoshii716@gmail.com>
2014-09-01 23:50:55 +0900

    add view test by selenium2 init

---
+++ fuel/app/tests/view/base.php
@@ -0,0 +1,27 @@
+<?php
+
+abstract class Test_View_Base extends \PHPUnit_Extensions_Selenium2TestCase
+{
+  public static $browsers = array(
+    array(
+      'browserName' => 'safari',
+      'host' => 'localhost',
+      'port' => 4444,
+    ),
+/*
+    array(
+      'browserName' => 'firefox',
+      'host' => 'localhost',
+      'port' => 4444,
+    ),
+*/
+  );
+
+  public static function setUpBeforeClass()
+  {
+    echo "\n\n";
+    echo "Seleniumを使ったテストを実行します。\n";
+    echo "このテストはSeleniumが起動していないとSkipされてしまいます。\n";
+    echo "\n";
+  }
+}

8e900d08c4de7dcfc42425ef8c8ce5fb9dba6ace
Takanao Yoshii <tyoshii716@gmail.com>
2014-09-02 21:00:24 +0900

    add Model_Test unittest

--- fuel/app/tests/model/team.php
+++ fuel/app/tests/model/team.php
@@ -17,8 +17,30 @@ class Test_Model_Team extends \Test_Model_Base
   {
   }
 
+  /**
+   *
+   */
   public function test_スキーマチェック()
   {
     $this->assertSchema();
+  }
+
+  /**
+   *
+   */
+  public function test_新規チーム登録()
+  {
+    // チーム名を与えない場合はfalse
+    $this->assertFalse(Model_Team::regist());
+
+    // 新規登録
+    $name = rand();
+    $id = Model_Team::regist($name);
+
+    // 登録したチームのモデル
+    $team = Model_Team::find($id);
+
+    $this->assertSame($id,   $team->id);
+    $this->assertSame($name, $team->name);
   }
 }

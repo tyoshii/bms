29da3925daf17f0efc343c73b47914e1690a3dcc
Takanao Yoshii <tyoshii716@gmail.com>
2014-09-03 12:28:11 +0900

    トップページからのログイン成功/失敗のテストを追加

--- fuel/app/tests/view/top.php
+++ fuel/app/tests/view/top.php
@@ -12,19 +12,35 @@ class Test_View_Top extends Test_View_Base
   protected function setUp()
   {
     $this->setBrowserUrl('http://localhost:8888/');
+
   }
 
-  public function test_ログインできる()
+  public function test_ログイン失敗()
   {
-    $this->waitUntil(function() {
-      $this->url('/');
-      $this->byName('username')->value('admin');
-      $this->byName('password')->value('adminadmin');
-      $this->byName('login')->submit();
+    // login
+    $this->url('/');
+    $this->byName('username')->value('cannot-login-user');
+    $this->byName('password')->value('dummy');
+    $this->byName('login')->submit();
 
-      return true;
-    }, 60000);
+    // ログイン失敗のメッセージ
+    $alert = $this->byCssSelector('div.alert[role=alert-error] span');
+    $this->assertSame('Error! ログインに失敗しました', $alert->text());
 
-    // TODO:どうやっててすと？
+    // ログイン専用ページに飛ばされる
+    $this->assertStringEndsWith('/login', $this->url());
+  }
+
+  public function test_ログイン成功()
+  {
+    // login
+    $this->url('/');
+    $this->byName('username')->value('user');
+    $this->byName('password')->value('password');
+    $this->byName('login')->submit();
+
+    // check
+    $alert = $this->byCssSelector('div.alert[role=alert-info] span');
+    $this->assertSame('Information ログインに成功しました！', $alert->text());
   }
 }

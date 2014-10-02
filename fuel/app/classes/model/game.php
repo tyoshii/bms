d23872e2e7fa151a6b12bb61801d89bf78dcd001
Takanao Yoshii <tyoshii716@gmail.com>
2014-10-03 03:38:57 +0900

    test追加

--- fuel/app/classes/model/game.php
+++ fuel/app/classes/model/game.php
@@ -14,6 +14,11 @@ class Model_Game extends \Orm\Model
 		'bottom_status'    => array('default' => 1),
 		'created_at',
 		'updated_at',
+		// TODO:削除予定
+		'team_top' => array('default' => 0),
+		'team_top_name' => array('default' => 0),
+		'team_bottom' => array('default' => 0),
+		'team_bottom_name' => array('default' => 0),
 	);
 
 	protected static $_observers = array(

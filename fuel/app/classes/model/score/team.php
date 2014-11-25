25a50e01c2cfe208445970710ff9226f46f8b016
Takanao Yoshii <tyoshii716@gmail.com>
2014-11-26 04:48:21 +0900

    勝率計算の不具合

--- fuel/app/classes/model/score/team.php
+++ fuel/app/classes/model/score/team.php
@@ -117,6 +117,7 @@ SELECT
 	g.id,
 	gr.tsum,
 	gr.bsum,
+	gt.order,
 	g.team_top,
 	g.team_bottom,
 	g.game_status,
@@ -171,11 +172,11 @@ __QUERY__;
 		{
 			if ($info['tsum'] > $info['bsum'])
 			{
-				$info['team_top'] == $team_id ? ++$ret['win'] : ++$ret['lose'];
+				$info['order'] == 'top' ? ++$ret['win'] : ++$ret['lose'];
 			}
 			elseif ($info['tsum'] < $info['bsum'])
 			{
-				$info['team_top'] == $team_id ? ++$ret['lose'] : ++$ret['win'];
+				$info['order'] == 'top' ? ++$ret['lose'] : ++$ret['win'];
 			}
 			else
 			{

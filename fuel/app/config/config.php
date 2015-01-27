44359031aa9d213ef8e1928562a75780e3e3fd13
Takanao Yoshii <tyoshii716@gmail.com>
2015-01-28 07:07:03 +0900

    when https cookie add secure attr

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -185,7 +185,7 @@ return array(
 	/**
 	 * Cookie settings
 	 */
-	// 'cookie' => array(
+	'cookie' => array(
 	// Number of seconds before the cookie expires
 	// 'expiration'  => 0,
 	// Restrict the path that the cookie is available to
@@ -193,10 +193,10 @@ return array(
 	// Restrict the domain that the cookie is available to
 	// 'domain'      => null,
 	// Only transmit cookies over secure connections
-	// 'secure'      => false,
+		'secure'      => true,
 	// Only transmit cookies over HTTP, disabling Javascript access
 	// 'http_only'   => false,
-	// ),
+	),
 
 	/**
 	 * Validation settings

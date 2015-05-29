8c7abf38628d6371521bc4634947a24dd9d0c68d
Takanao Yoshii <tyoshii@yahoo-corp.jp>
2015-05-29 15:30:08 +0900

    sample team_url static value get from Test_Base class

--- fuel/app/tests/controller/team/base.php
+++ fuel/app/tests/controller/team/base.php
@@ -2,12 +2,4 @@
 
 abstract class Test_Controller_Team_Base extends Test_Base
 {
-	public static $team_url = '';
-
-	public static function setUpBeforeClass()
-	{
-		parent::setUpBeforeClass();
-
-		self::$team_url = 'team/'.Model_Team::find('first')->url_path;
-	}
 }

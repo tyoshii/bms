a33a396fd866cfeea30f4f14e0f39ab1d3eac958
Takanao Yoshii <tyoshii716@gmail.com>
2016-06-18 01:06:32 +0900

    update selenium test

--- fuel/app/tests/view/top.php
+++ fuel/app/tests/view/top.php
@@ -14,25 +14,4 @@ class Test_View_Top extends Test_View_Base
 		$this->setBrowserUrl('http://localhost:8888/');
 
 	}
-
-	public function test_ログイン成功()
-	{
-		// TODO
-	}
-
-	public function test_ログイン失敗()
-	{
-		// login
-		$this->url('/');
-		$this->byName('email')->value('cannot-login-user');
-		$this->byName('password')->value('dummy');
-		$this->byName('login')->submit();
-
-		// ログイン失敗のメッセージ
-		$alert = $this->byCssSelector('div.alert[role=alert-error] span');
-		$this->assertSame('Error! ログインに失敗しました', $alert->text());
-
-		// ログイン専用ページに飛ばされる
-		$this->assertStringEndsWith('/login', $this->url());
-	}
 }
